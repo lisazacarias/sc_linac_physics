a314c7b3e76420d25afda71b07903697ade39a35
Lisa Zacarias <zacarias@slac.stanford.edu>
2024-08-21 15:29:43 -0700

    launches, but most things not displayed in UI. still need to add some of the commented stuff back in

--- applications/quench_processing/quench_gui.py
+++ applications/quench_processing/quench_gui.py
@@ -2,7 +2,7 @@ from functools import partial
 from typing import Dict
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QHBoxLayout, QComboBox, QVBoxLayout, QGroupBox
+from PyQt5.QtWidgets import QHBoxLayout, QComboBox, QVBoxLayout, QGroupBox, QPushButton
 from epics import camonitor, camonitor_clear
 from lcls_tools.common.frontend.plotting.util import (
     WaveformPlotParams,
@@ -17,6 +17,7 @@ from qtpy.QtCore import Signal, Slot
 from applications.quench_processing.quench_linac import QUENCH_MACHINE, QuenchCavity
 from utils.sc_linac.cryomodule import Cryomodule
 from utils.sc_linac.linac_utils import ALL_CRYOMODULES
+from utils.widgets.rf_controls import RFControls
 
 
 class QuenchGUI(Display):
@@ -41,6 +42,10 @@ class QuenchGUI(Display):
         self.cm_combobox.addItems(ALL_CRYOMODULES)
         self.cav_combobox.addItems(map(str, range(1, 9)))
         self.decarad_combobox.addItems(["1", "2"])
+
+        self.rf_controls = RFControls()
+        self.setup_button: QPushButton = QPushButton("Setup for Processing")
+        self.reset_button: QPushButton = QPushButton("Reset")
 
         self.current_cm = None
         self.current_cav = None
@@ -81,12 +86,12 @@ class QuenchGUI(Display):
 
     def clear_all_connections(self):
         for signal in [
-            self.ui.button_ssa_on.clicked,
-            self.ui.button_ssa_off.clicked,
-            self.ui.button_rf_on.clicked,
-            self.ui.button_rf_off.clicked,
-            self.ui.setup_button.clicked,
-            self.ui.reset_button.clicked,
+            self.rf_controls.ssa_on_button.clicked,
+            self.rf_controls.ssa_off_button.clicked,
+            self.rf_controls.rf_on_button.clicked,
+            self.rf_controls.rf_off_button.clicked,
+            self.setup_button.clicked,
+            self.reset_button.clicked,
         ]:
             self.clear_connections(signal)
 
@@ -103,34 +108,34 @@ class QuenchGUI(Display):
             int(self.cav_combobox.currentText())
         ]
 
-        self.ui.button_ssa_on.clicked.connect(self.current_cav.ssa.turn_on)
-        self.ui.button_ssa_off.clicked.connect(self.current_cav.ssa.turn_off)
-        self.ui.label_ssa_status_rdbk.channel = self.current_cav.ssa.status_pv
+        self.rf_controls.ssa_on_button.clicked.connect(self.current_cav.ssa.turn_on)
+        self.rf_controls.ssa_off_button.clicked.connect(self.current_cav.ssa.turn_off)
+        self.rf_controls.ssa_readback_label.channel = self.current_cav.ssa.status_pv
 
-        self.ui.combobox_rfmode.channel = self.current_cav.rf_control_pv
-        self.ui.label_rfmode_rdbk.channel = self.current_cav.rf_mode_pv
-        self.ui.button_rf_on.clicked.connect(self.current_cav.turn_on)
-        self.ui.button_rf_off.clicked.connect(self.current_cav.turnOff)
-        self.ui.label_rfstatus_rdbk.channel = self.current_cav.rf_state_pv
-        self.ui.ades_spinbox.channel = self.current_cav.ades_pv
-        self.ui.ades_readback_label.channel = self.current_cav.aact_pv
-        self.ui.setup_button.clicked.connect(self.current_cav.setup_sela)
-        self.ui.srf_max_spinbox.channel = self.current_cav.srf_max_pv
-        self.ui.srf_max_label.channel = self.current_cav.srf_max_pv
+        self.rf_controls.rf_mode_combobox.channel = self.current_cav.rf_control_pv
+        self.rf_controls.rf_mode_readback_label.channel = self.current_cav.rf_mode_pv
+        self.rf_controls.rf_on_button.clicked.connect(self.current_cav.turn_on)
+        self.rf_controls.rf_off_button.clicked.connect(self.current_cav.turn_off)
+        self.rf_controls.rf_status_readback_label.channel = self.current_cav.rf_state_pv
+        self.rf_controls.ades_spinbox.channel = self.current_cav.ades_pv
+        self.rf_controls.aact_readback_label.channel = self.current_cav.aact_pv
+        self.setup_button.clicked.connect(self.current_cav.setup_sela)
+        self.rf_controls.srf_max_spinbox.channel = self.current_cav.srf_max_pv
+        self.rf_controls.srf_max_readback_label.channel = self.current_cav.srf_max_pv
 
-        self.ui.cav_power_label.channel = self.current_cav.cav_power_pv
-        self.ui.forward_power_label.channel = self.current_cav.forward_power_pv
-        self.ui.rev_power_label.channel = self.current_cav.reverse_power_pv
+        # self.ui.cav_power_label.channel = self.current_cav.cav_power_pv
+        # self.ui.forward_power_label.channel = self.current_cav.forward_power_pv
+        # self.ui.rev_power_label.channel = self.current_cav.reverse_power_pv
+        #
+        # self.ui.latch_indicator.channel = self.current_cav.quench_latch_pv
+        # self.ui.latch_label.channel = self.current_cav.quench_latch_pv
+        #
+        # self.ui.bypass_button.channel = self.current_cav.quench_bypass_pv
+        # self.ui.unbypass_button.channel = self.current_cav.quench_bypass_pv
+        # self.ui.bypass_indicator.channel = self.current_cav.quench_bypass_pv + "_RBV"
+        # self.ui.bypass_label.channel = self.current_cav.quench_bypass_pv + "_RBV"
 
-        self.ui.latch_indicator.channel = self.current_cav.quench_latch_pv
-        self.ui.latch_label.channel = self.current_cav.quench_latch_pv
-
-        self.ui.bypass_button.channel = self.current_cav.quench_bypass_pv
-        self.ui.unbypass_button.channel = self.current_cav.quench_bypass_pv
-        self.ui.bypass_indicator.channel = self.current_cav.quench_bypass_pv + "_RBV"
-        self.ui.bypass_label.channel = self.current_cav.quench_bypass_pv + "_RBV"
-
-        self.ui.reset_button.clicked.connect(
+        self.reset_button.clicked.connect(
             partial(self.current_cav.reset_interlocks, True)
         )
 
--- utils/widgets/rf_controls.py
+++ utils/widgets/rf_controls.py
@@ -1,12 +1,22 @@
-from PyQt5.QtWidgets import QPushButton, QComboBox
-from pydm.widgets import PyDMEnumComboBox, PyDMSpinbox
+from PyQt5.QtWidgets import QPushButton
+from pydm.widgets import PyDMEnumComboBox, PyDMSpinbox, PyDMLabel
 
 
 class RFControls:
     def __init__(self):
         self.ssa_on_button: QPushButton = QPushButton("On")
         self.ssa_off_button: QPushButton = QPushButton("Off")
+        self.ssa_readback_label: PyDMLabel = PyDMLabel()
+
         self.rf_mode_combobox: PyDMEnumComboBox = PyDMEnumComboBox()
+        self.rf_mode_readback_label: PyDMLabel = PyDMLabel()
+
         self.rf_on_button: QPushButton = QPushButton("On")
         self.rf_off_button: QPushButton = QPushButton("Off")
+        self.rf_status_readback_label: PyDMLabel = PyDMLabel()
+
         self.ades_spinbox: PyDMSpinbox = PyDMSpinbox()
+        self.aact_readback_label: PyDMLabel = PyDMLabel()
+
+        self.srf_max_spinbox: PyDMSpinbox = PyDMSpinbox()
+        self.srf_max_readback_label: PyDMLabel = PyDMLabel()
